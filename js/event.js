fe94f4ffc7f5887e2c21c22c1ec6df844e7a6b0e
mim2041 <suraiyamim338@gmail.com>
2023-02-10 19:45:25 +0600

    Another Module of DOM. Events are added to DOM

---
+++ js/event.js
@@ -0,0 +1,33 @@
+console.log('This is separate js file');
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';       
+}
+
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';
+}
+
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click',makePink)
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
+
+// important, we will use this
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
